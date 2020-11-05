7ca0cd53332a9926dd5618a4bbfff9e14a255e15
Magne Alvheim <magne@alvheim.com>
2020-11-05 16:17:26 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const cors = require('cors')
+const express = require('express')
+const app = express()
+app.use(cors())
+
+const Database = require('@replit/database')
+const db = new Database();
+db.set("1", {firstname: 'Magne', lastname: 'Alvheim'}).then(() => {}) 
+db.set("2", {firstname: 'Bill', lastname: 'Gates'}).then(() => {}) 
+db.set("3", {firstname: 'Steve', lastname: 'Jobs'}).then(() => {}) 
+
+app.get('/', (req, res) => {res.send('Ulriken Consulting is cooking on gas!')})
+
+app.get('/users', async (req, res) => {
+  let all = await db.getAll()
+  let result = Object.entries(all)
+  res.json(result)
+ })
+
+app.get('/user/:id', async (req, res) => {
+  console.log(req.params.id)
+  let result = await db.get(req.params.id)  
+  res.json(result)
+ })
+
+
+app.listen(3000, () => console.log('App på port 3000'))
+
+ 
